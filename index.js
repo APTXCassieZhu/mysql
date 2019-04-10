973e01da62b6dbf66e9bbf5f05ef24a7171f192f
Ubuntu <ubuntu@hw7.cloud.compas.cs.stonybrook.edu>
2019-04-10 20:36:04 +0000

    connected to mysql: add new user

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 const app = express();
 var mysql = require('mysql');
+const port = 3000;
 
 var con = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: ""
+    user: "newuser",
+    password: "password"
 });
-  
+
 con.connect(function(err) {
     if (err)
         console.log(err);
@@ -18,4 +19,7 @@ app.get('/',function (req, res, next) {
     res.send("OK");
 });
 
-app.listen(80);
+//app.listen(80);
+app.listen(port,'0.0.0.0', () => {
+    return console.log(`App listening on port ${port}!`);
+})
