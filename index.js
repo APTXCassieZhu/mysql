17713809f0e044d7aaba5e1c0a4b054c5a7df49f
ruiqi <ruiqi@appledeMacBook-Air-2.local>
2019-04-10 20:32:42 -0400

    step1-step4 success

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/hw7', function(req, res, next){
         console.log(result);
         console.log(result[0]);
         return res.json({club:req.query.club, pos: req.query.pos, max_assists: result[0][0].A, 
-            player: result[0][0].player, avg_assists: result[1][0]["AVG(A)"]});
+            player: result[0][0].Player, avg_assists: result[1][0]["AVG(A)"]});
       });
 });
 
