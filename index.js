38cdebb8d8c508dbfe5435a768c1f26ee3b73ef3
ruiqi <ruiqi@appledeMacBook-Air-2.local>
2019-04-10 20:00:28 -0400

    see the select table

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get('/', function(req, res, next) {
 app.get('/hw7', function(req, res, next){
     // get the max assist
     var query = "SELECT * FROM assists WHERE Club = '?' AND POS = '?' ORDER BY A DESC, GS DESC";
+    console.log(req.body);
     con.query(query, [req.body.club, req.body.pos], function (err, result, fields) {
         if (err) 
             console.log(err); 
@@ -34,8 +35,8 @@ app.get('/hw7', function(req, res, next){
       });
 });
 
-//app.listen(port,'0.0.0.0', () => {
-//    return console.log(`App listening on port ${port}!`);
-//})
 app.listen(80);
+/*app.listen(port,'0.0.0.0', () => {
+    return console.log(`App listening on port ${port}!`);
+})*/
 
