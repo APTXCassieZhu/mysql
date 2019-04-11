c1f80d1c3c8883570decb8e615fa58908542a45d
ruiqi <ruiqi@appledeMacBook-Air-2.local>
2019-04-10 20:25:13 -0400

    fix execute queries at the same time2.0

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.get('/', function(req, res, next) {
 
 app.get('/hw7', function(req, res, next){
     // get the max assist
-    var query = "SELECT * FROM assists WHERE Club = ? AND POS = ? ORDER BY A DESC, GS DESC";
-    query += "SELECT AVG(A) FROM (SELECT * FROM assists WHERE Club= ? AND POS = ?) as avg";
+    var query = "SELECT * FROM assists WHERE Club = ? AND POS = ? ORDER BY A DESC, GS DESC;";
+    query += "SELECT AVG(A) FROM (SELECT * FROM assists WHERE Club= ? AND POS = ?) as avg;";
     con.query(query, [req.query.club, req.query.pos, req.query.club, req.query.pos], function (err, result, fields) {
         if (err) 
             console.log(err); 
