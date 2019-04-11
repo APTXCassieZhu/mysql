f363fb180da019d6938609845d31cc6f50c5261e
ruiqi <ruiqi@appledeMacBook-Air-2.local>
2019-04-10 22:06:10 -0400

    try memcache

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ con.connect(function(err) {
     console.log("Connected!");
 });
 
+memcached.set('key','value01');
+memcached.get('key', function(err, data){
+    console.log("data is "+data);
+});
+
+
+
 app.get('/', function(req, res, next) {
     res.send("OK");
 });
